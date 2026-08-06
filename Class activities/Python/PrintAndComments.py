36a762fe76c63a7c7e32a86e0831c26ae7c1263d
TanyaCodingal <tanya81972@gmail.com>
2026-08-06 20:39:24 +0530

    Python day 1

---
+++ Class activities/Python/PrintAndComments.py
@@ -0,0 +1,6 @@
+#print with string argument
+print("Welcome to Python programming")#String kind of argument
+#print with integer argument
+print(45)
+#print with different arguments
+print("Addition : ",10 + 20)
