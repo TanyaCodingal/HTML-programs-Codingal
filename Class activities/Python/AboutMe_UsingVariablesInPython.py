eb122dea71f82eea2b3aeadc8cec94a4cbb86a48
TanyaCodingal <tanya81972@gmail.com>
2026-08-09 19:20:47 +0530

    Using Variables in Python

---
+++ Class activities/Python/AboutMe_UsingVariablesInPython.py
@@ -0,0 +1,10 @@
+name="Tanya"
+age=12
+height=5.4
+isStudent=True
+hobby=input("Enter your hobby : ")
+print("\n\n\n\n\nName : ",name)
+print("Age : ",age)
+print("Height :",height)
+print("Student : ",isStudent)
+print("Hobby : ",hobby)
